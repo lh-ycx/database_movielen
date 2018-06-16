09c462a9551c8eb5361534e1e3d59129f7f11abe
PKULiuHui <xinkeliuhui@pku.edu.cn>
2018-06-16 16:50:32 +0800

    Revert "增加基本数据分析部分"
    This reverts commit 858b0d79385160f71408b6b8f84fb6ed2a79c3b9.

--- stage_2/Q1.sql
+++
@@ -1,3 +0,0 @@
---统计每个genre下的电影数量
-use movielen;
-select genre, count(*) as movie_num from movie_genre group by genre
--- stage_2/Q2.sql
+++
@@ -1,7 +0,0 @@
---统计每个genre下的平均用户评分
-use movielen;
-
-select genre, avg(rating) as avg_rating
-from movie_genre, Ratings
-where movie_genre.movieid = Ratings.movieid
-group by genre
