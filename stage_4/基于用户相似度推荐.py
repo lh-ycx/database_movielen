fc73b11b603bb9bfdc2f238912859171dcdc40ca
lh-ycx <lh_ycx@126.com>
2018-07-03 13:22:04 +0800

    完成stage4.2 基于用户相似度的推荐

--- stage_4/基于用户相似度推荐.py
+++ stage_4/基于用户相似度推荐.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     usrid = input("please input the usrid(from 211914 to 221853):")
 
     #寻找于该用户最相近的三个用户
-    reslist = ms.ExecQuery("""  select top 3 PID_2
+    reslist = ms.ExecQuery("""  select top 3 PID_2, similarity
                                 from dbo.Usr_Similarity
                                 where PID_1 = """+ usrid +
                                 "order by similarity desc")
@@ -52,9 +52,25 @@ if __name__ == '__main__':
 
     #推荐他还没看过的其他人看的高分电影，每一个相似的用户推荐前3个
     if reslist :
-        print(reslist)
         for row in reslist:
-
+            print('usrid:' + str(row['PID_2']) + '    similarity:'+str(row['similarity']))
+            tempres = ms.ExecQuery("""  select top 3 movie_title_pub_date.movieId, title,rating 
+                                        from dbo.movie_title_pub_date,dbo.Ratings
+                                        where movie_title_pub_date.movieId = Ratings.movieId 
+                                        and not exists (
+                                            select * from Ratings T
+                                            where T.movieId = movie_title_pub_date.movieId
+                                            and T.userId = """ + usrid +
+                                        """
+                                        )
+                                        and Ratings.userId = """ + str(row['PID_2']) +
+                                        " order by rating desc")
+            if tempres:
+                print('movieId\ttitle\trating')
+                for res in tempres:
+                    print(res['movieId'],res['title'],res['rating'])
+            else :
+                print('no movie to recommand!')
     
     else :
         print('user '+usrid+' does not exist!')
